11a236aa0ffbfe9fa3459f5939fdbfc3758f4ded
angel <rcmo202029@gmail.com>
2025-10-01 17:36:16 -0600

    Arreglos de importacion

--- resources/js/Components/UserCard.vue
+++ resources/js/Components/UserCard.vue
@@ -2,10 +2,10 @@
 import { computed, ref } from "vue";
 import { useMainStore } from "@/stores/main";
 import { mdiCheckDecagram } from "@mdi/js";
-import BaseLevel from "@/components/BaseLevel.vue";
+import BaseLevel from "@/Components/BaseLevel.vue";
 import UserAvatarCurrentUser from "@/components/UserAvatarCurrentUser.vue";
-import CardBox from "@/components/CardBox.vue";
-import FormCheckRadio from "@/components/FormCheckRadio.vue";
+import CardBox from "@/Components/CardBox.vue";
+import FormCheckRadio from "@/Components/FormCheckRadio.vue";
 import PillTag from "@/components/PillTag.vue";
 
 const mainStore = useMainStore();
